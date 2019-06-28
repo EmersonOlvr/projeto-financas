8017a155bfee5d08e367c08fd52ce0c25ca7044d
lassouz4 <lais.souza1962@gmail.com>
2019-06-28 14:21:07 -0300

    Adicionado: cadastro de despesa

--- src/main/java/com/jolteam/financas/controller/DespesaController.java
+++ src/main/java/com/jolteam/financas/controller/DespesaController.java
@@ -1,15 +1,65 @@
 package com.jolteam.financas.controller;
 
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jolteam.financas.dao.CategoriaDAO;
+import com.jolteam.financas.dao.UsuarioDAO;
+import com.jolteam.financas.exceptions.DespesaException;
+import com.jolteam.financas.model.Despesa;
+import com.jolteam.financas.model.Log;
+import com.jolteam.financas.model.TiposLogs;
+import com.jolteam.financas.model.TiposTransacoes;
+import com.jolteam.financas.service.DespesaService;
+import com.jolteam.financas.service.LogService;
 
 @Controller
 public class DespesaController {
-
+	
+	@Autowired private DespesaService despesaService;
+	@Autowired private LogService logService;
+	@Autowired private CategoriaDAO categorias;
+	@Autowired private UsuarioDAO usuarios;
+	
+	//
+	
 	@GetMapping("/despesas/adicionar")
-	public String viewAdicionarDespesa() {
-		return "/despesas-adicionar";
+	public ModelAndView viewDespesaAdicionar() {
+			ModelAndView mv=new ModelAndView("/despesas-adicionar");
+			mv.addObject("despesa", new Despesa());
+			mv.addObject("categorias", this.categorias.findByUsuarioAndTipoTransacao(this.usuarios.getOne(1), TiposTransacoes.DESPESA));
+			return mv;
 	}
+	@PostMapping("/despesas/adicionar")
+	public ModelAndView adicionarDespesa(@ModelAttribute Despesa despesa,HttpServletRequest request) {
+		try {
+			this.despesaService.salvar(despesa);
+			try {
+				// salva um log de sucesso no banco
+				this.logService.salvar(new Log(despesa.getUsuario(), TiposLogs.CADASTRO_DESPESA, LocalDateTime.now(), request.getRemoteAddr()));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}catch (DespesaException de) {
+			try {
+				// salva um log de erro no banco
+				this.logService.salvar(new Log(despesa.getUsuario(), TiposLogs.ERRO_CADASTRO_DESPESA, LocalDateTime.now(), request.getRemoteAddr()));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			return this.viewDespesaAdicionar().addObject("erro", de.getMessage());
+		}
+		return this.viewDespesaAdicionar().addObject("sucesso", "Despesa salva com sucesso.");
+	}
+	
 	
 	@GetMapping("/despesas/categorias")
 	public String viewCategoriasDespesas() {
---
+++ src/main/java/com/jolteam/financas/model/Despesa.java
@@ -0,0 +1,60 @@
+package com.jolteam.financas.model;
+
+import java.math.BigDecimal;
+
+public class Despesa {
+
+	private Usuario usuario;
+	private Categoria categoria;
+	private String descricao;
+	private BigDecimal valor;
+	
+	//Construtores
+	public Despesa() {}
+	
+	public Despesa(Usuario usuario,Categoria categoria,BigDecimal valor,String descricao) {
+		this.usuario=usuario;
+		this.categoria=categoria;
+		this.valor=valor;
+		this.descricao=descricao;
+	}
+	//Getters e Setters
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Despesa [usuario=" + usuario + ", categoria=" + categoria + ", descricao=" + descricao + ", valor="
+				+ valor + "]";
+	}
+	
+}
