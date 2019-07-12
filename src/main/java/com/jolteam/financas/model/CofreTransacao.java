920b9e0e7f6dd11add29f3028bcf097b4ff60d28
lassouz4 <lais.souza1962@gmail.com>
2019-07-12 16:42:41 -0300

    Adicionando:CofreTransação e Mapeando cofre;

---
+++ src/main/java/com/jolteam/financas/model/CofreTransacao.java
@@ -0,0 +1,78 @@
+package com.jolteam.financas.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class CofreTransacao {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne(optional = false)
+	private Cofre cofre;
+	
+	@ManyToOne(optional = false)
+	private Categoria categoria;
+	
+	@Column(nullable = false)
+	private BigDecimal valor;
+	
+	@Column(nullable = false, columnDefinition = "datetime")
+	private LocalDateTime dataCriacao;
+	
+	@Column(nullable = false,length = 150)
+	private String descricao;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Cofre getCofre() {
+		return cofre;
+	}
+	public void setCofre(Cofre cofre) {
+		this.cofre = cofre;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return "CofreTransacao [id=" + id + ", cofre=" + cofre + ", categoria=" + categoria + ", valor=" + valor
+				+ ", dataCriacao=" + dataCriacao + ", descricao=" + descricao + "]";
+	}
+	
+
+}
