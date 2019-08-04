e2b02aa5f1bf031314a310da164782160f513931
EmersonOlvr <emersonolvrsouza@gmail.com>
2019-08-03 22:38:02 -0300

    A cor de fundo da lista de movimentos agora corresponde ao saldo atual

--- src/main/java/com/jolteam/financas/dao/CofreTransacaoDAO.java
+++ src/main/java/com/jolteam/financas/dao/CofreTransacaoDAO.java
@@ -3,12 +3,10 @@ package com.jolteam.financas.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jolteam.financas.model.Cofre;
 import com.jolteam.financas.model.CofreTransacao;
-import com.jolteam.financas.model.Usuario;
 
 @Repository
 public interface CofreTransacaoDAO extends JpaRepository<CofreTransacao, Integer> {
